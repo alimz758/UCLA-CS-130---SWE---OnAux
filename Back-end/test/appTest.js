9425216afa87bb66d92d81375fcc26ffd8b818a0
Jake Wallin <jakewallin82@gmail.com>
2020-12-04 23:11:45 -0800

    More test cases

--- Back-end/test/appTest.js
+++ Back-end/test/appTest.js
@@ -8,44 +8,54 @@ const {v4: uuidv4} = require("uuid");
 let token = null;
 let user_id = null;
 let session_id = null;
+let _username = `${uuidv4()}`;
+let _firstName = "u5s";
+let _lastName = "u666s";
+let _email = `${uuidv4()}`;
+let _password = `12d34d`;
+let _sessionName = 'Jakes Session';
+let songuri =  "849846161";
 
 describe("GET /session/create", function () {
-    it("should say Hello", async function(){
+    it("Should say Hello", async function(){
 	const response = await request.get("/session/create");
 	console.log(response['text']);
 	assert.equal(response['text'], "Hello");
     });
 });
+
 describe("POST /user/signup", function () {
   it("User sign up", async function () {
     const response = await request
       .post("/user/signup")
-      .send({ username: `u8d82dd885${uuidv4()}s`, firstName : "u5s",
-      lastName : "u666s",
-      email: `usedd2r${uuidv4()}@gmail.com`,
-      password : "12d34d" });
-    console.log(response);
+      .send({ username: _username, firstName : _firstName,
+      lastName : _lastName,
+      email: _email,
+      password : _password});
+    console.log(response['text']);
     token = response['body']['token'];
     user_id = response['body']['user']['_id'];
-    console.log(token);
-    expect(response.status).to.eql(201);
-
-    //const attributes = response.body.data.attributes;
-    //expect(attributes).to.include.keys("kilometers", "miles", "nautical_miles");
-    //expect(attributes.kilometers).to.eql(8692.066508240026);
-    //expect(attributes.miles).to.eql(5397.239853492001);
-    //expect(attributes.nautical_miles).to.eql(4690.070954910584);
+    //console.log(token);
+    console.log(JSON.parse(response['text'])['user']['createdAt']);
+    expect(response.status).to.eql(201); // make sure correct status, then check for correct information
+    expect(JSON.parse(response['text'])['user']['username']).to.eql(_username);
+    expect(JSON.parse(response['text'])['user']['firstName']).to.eql(_firstName);
+    expect(JSON.parse(response['text'])['user']['lastName']).to.eql(_lastName);
+    expect(JSON.parse(response['text'])['user']['email']).to.eql(_email);
+    //Important - must make sure that a valid token has been returned to the user
+    assert(token.length > 0);
   });
 });
 
 describe("POST /session/create", function () {
-  it("User sign up", async function () {
+  it("Create Session", async function () {
     const response = await request
       .post("/session/create")
-      .send({ sessionName: "session"})
+      .send({ sessionName: _sessionName})
       .set("Authorization", "Bearer " + token);
     session_id = response['body']['newSessionInfo']['_id'];
-    console.log(response);
+    console.log(response['text']);
+    expect(JSON.parse(response['text'])['newSessionInfo']['sessionName']).to.eql(_sessionName);
 
     expect(response.status).to.eql(201);
 
@@ -68,8 +78,12 @@ describe("POST /session/session-id=sessionID/set-current-song", function () {
          "album" : "Single"
       }
   }).set("Authorization", "Bearer " + token);
-    console.log(response);
-
+    console.log(response['text']);
+    expect(JSON.parse(response['text'])['currentSongInfo']['songuri']).to.eql( "849846161");
+    expect(JSON.parse(response['text'])['currentSongInfo']['songName']).to.eql( "Sad Sad City");
+    expect(JSON.parse(response['text'])['currentSongInfo']['artist']).to.eql( "Ghostland Observatory");
+    expect(JSON.parse(response['text'])['currentSongInfo']['album']).to.eql( "Single");
+    expect(JSON.parse(response['text'])['sessionName']).to.eql(_sessionName);
     expect(response.status).to.eql(200);
 
     //const attributes = response.body.data.attributes;
@@ -81,7 +95,7 @@ describe("POST /session/session-id=sessionID/set-current-song", function () {
 });
 
 describe("First try: POST /session/session-id=sessionID/history/add-song", function () {
-  it("User sign up", async function () {
+  it("Adds a song to session's history", async function () {
     const response = await request
       .post(`/session/session-id=${session_id}/history/add-song`)
       .send({ songInfo: {
@@ -91,8 +105,11 @@ describe("First try: POST /session/session-id=sessionID/history/add-song", funct
          "album" : "Single"
       }
   }).set("Authorization", "Bearer " + token);
-    console.log(response);
-
+    console.log(response['text']);
+    expect(JSON.parse(response['text'])[0]['songuri']).to.eql( "849846161");
+    expect(JSON.parse(response['text'])[0]['songName']).to.eql( "Sad Sad City");
+    expect(JSON.parse(response['text'])[0]['artist']).to.eql( "Ghostland Observatory");
+    expect(JSON.parse(response['text'])[0]['album']).to.eql( "Single");
     expect(response.status).to.eql(200);
 
     //const attributes = response.body.data.attributes;
@@ -103,8 +120,8 @@ describe("First try: POST /session/session-id=sessionID/history/add-song", funct
   });
 });
 
-describe("First try: POST /session/session-id=sessionID/history/add-song", function () {
-  it("User sign up", async function () {
+describe("Second try: POST /session/session-id=sessionID/history/add-song", function () {
+  it("Adds another song to session's history", async function () {
     const response = await request
       .post(`/session/session-id=${session_id}/history/add-song`)
       .send({ songInfo:  {
@@ -114,8 +131,15 @@ describe("First try: POST /session/session-id=sessionID/history/add-song", funct
         "album": "Single"
     }
   }).set("Authorization", "Bearer " + token);
-    console.log(response);
-
+    console.log(response['text']);
+    expect(JSON.parse(response['text'])[0]['songuri']).to.eql( "849846161");
+    expect(JSON.parse(response['text'])[0]['songName']).to.eql( "Sad Sad City");
+    expect(JSON.parse(response['text'])[0]['artist']).to.eql( "Ghostland Observatory");
+    expect(JSON.parse(response['text'])[0]['album']).to.eql( "Single");
+    expect(JSON.parse(response['text'])[1]['songuri']).to.eql( "849846121");
+    expect(JSON.parse(response['text'])[1]['songName']).to.eql("Bad & Boujee");
+    expect(JSON.parse(response['text'])[1]['artist']).to.eql( "Migos");
+    expect(JSON.parse(response['text'])[1]['album']).to.eql( "Single");
     expect(response.status).to.eql(200);
 
     //const attributes = response.body.data.attributes;
@@ -126,8 +150,8 @@ describe("First try: POST /session/session-id=sessionID/history/add-song", funct
   });
 });
 
-describe("First try: POST /session/session-id=sessionID/history/add-song", function () {
-  it("User sign up", async function () {
+describe("Third try: POST /session/session-id=sessionID/history/add-song", function () {
+  it("Adds another song to session's history", async function () {
     const response = await request
       .post(`/session/session-id=${session_id}/history/add-song`)
       .send({ songInfo:  {
@@ -137,8 +161,16 @@ describe("First try: POST /session/session-id=sessionID/history/add-song", funct
         "album": "Single"
     }
   }).set("Authorization", "Bearer " + token);
-    console.log(response);
-
+    console.log(response['text']);
+    expect(JSON.parse(response['text'])[0]['songuri']).to.eql( "849846161");
+    expect(JSON.parse(response['text'])[0]['songName']).to.eql( "Sad Sad City");
+    expect(JSON.parse(response['text'])[0]['artist']).to.eql( "Ghostland Observatory");
+    expect(JSON.parse(response['text'])[0]['album']).to.eql( "Single");
+    expect(JSON.parse(response['text'])[1]['songuri']).to.eql( "849846121");
+    expect(JSON.parse(response['text'])[1]['songName']).to.eql("Bad & Boujee");
+    expect(JSON.parse(response['text'])[1]['artist']).to.eql( "Migos");
+    expect(JSON.parse(response['text'])[1]['album']).to.eql( "Single");
+    assert(JSON.parse(response['text']).length == 2);
     expect(response.status).to.eql(200);
 
     //const attributes = response.body.data.attributes;
@@ -150,12 +182,20 @@ describe("First try: POST /session/session-id=sessionID/history/add-song", funct
 });
 
 describe("GET /session/session-id=sessionID/history", function () {
-  it("User sign up", async function () {
+  it("View the session's history", async function () {
     const response = await request
       .get(`/session/session-id=${session_id}/history/`)
       .set("Authorization", "Bearer " + token);
-    console.log(response);
-
+    console.log(response['text']);
+    expect(JSON.parse(response['text'])[0]['songuri']).to.eql( "849846161");
+    expect(JSON.parse(response['text'])[0]['songName']).to.eql( "Sad Sad City");
+    expect(JSON.parse(response['text'])[0]['artist']).to.eql( "Ghostland Observatory");
+    expect(JSON.parse(response['text'])[0]['album']).to.eql( "Single");
+    expect(JSON.parse(response['text'])[1]['songuri']).to.eql( "849846121");
+    expect(JSON.parse(response['text'])[1]['songName']).to.eql("Bad & Boujee");
+    expect(JSON.parse(response['text'])[1]['artist']).to.eql( "Migos");
+    expect(JSON.parse(response['text'])[1]['album']).to.eql( "Single");
+    assert(JSON.parse(response['text']).length == 2);
     expect(response.status).to.eql(200);
 
     //const attributes = response.body.data.attributes;
@@ -166,32 +206,64 @@ describe("GET /session/session-id=sessionID/history", function () {
   });
 });
 
-describe("POST /session/session-id=sessionID/history", function () {
-  it("User sign up", async function () {
+describe("POST /session/session-id=sessionID/request-song", function () {
+  it("Request a song to be in the Session", async function () {
     const response = await request
-      .get(`/session/session-id=${session_id}/request-song`)
-      .send({ songInfo:  {
-        "songuri": "849846121",
-        "songName": "Bad & Boujee",
-        "artist": "Migos",
-        "album": "Single"
-  	  	}
-  	}).set("Authorization", "Bearer " + token);
-    console.log(response);
-
+      .post(`/session/session-id=${session_id}/request-song`)
+      .send(   {
+      "songInfo": {
+         "songuri" : "84938463",
+         "songName" : "Bad & Boujee",
+         "artist" : "Migos",
+         "album" : "Single"
+      },
+      "vote" : "1"
+   }).set("Authorization", "Bearer " + token);
+    console.log(response['text']);
     expect(response.status).to.eql(200);
-
-    //const attributes = response.body.data.attributes;
-    //expect(attributes).to.include.keys("kilometers", "miles", "nautical_miles");
-    //expect(attributes.kilometers).to.eql(8692.066508240026);
-    //expect(attributes.miles).to.eql(5397.239853492001);
-    //expect(attributes.nautical_miles).to.eql(4690.070954910584);
   });
 });
 
+describe("POST /session/session-id=sessionID/request-song", function () {
+  it("Request a song to be in the Session", async function () {
+    const response = await request
+      .post(`/session/session-id=${session_id}/request-song`)
+      .send(   {
+      "songInfo": {
+         "songuri" : "84938463",
+         "songName" : "Bad & Boujee",
+         "artist" : "Migos",
+         "album" : "Single"
+      },
+      "vote" : "1"
+   }).set("Authorization", "Bearer " + token);
+    console.log(response['text']);
+
+    expect(response.status).to.eql(200);
+  });
+});
+
+describe("POST /session/session-id=sessionID/request-song", function () {
+  it("Request a song to be in the Session", async function () {
+    const response = await request
+      .post(`/session/session-id=${session_id}/request-song`)
+      .send(   {
+      "songInfo": {
+         "songuri" : "84938463",
+         "songName" : "Bad & Boujee",
+         "artist" : "Migos",
+         "album" : "Single"
+      },
+      "vote" : "1"
+   }).set("Authorization", "Bearer " + token);
+    console.log(response['text']);
+    console.log(JSON.parse(response['text'])['createdAt']);
+    expect(response.status).to.eql(200);
+  });
+});
 
 describe("POST /user/add-song", function () {
-  it("User sign up", async function () {
+  it("Add a song to User's Liked Songs", async function () {
     const response = await request
       .post(`/user/add-song`)
       .send({   songInfo: {
@@ -201,8 +273,8 @@ describe("POST /user/add-song", function () {
          "album" : "Single"
       }
   	}).set("Authorization", "Bearer " + token);
-    console.log(response);
-
+    console.log(response['text']);
+    console.log(response['text']['createdAt']);
     expect(response.status).to.eql(200);
 
     //const attributes = response.body.data.attributes;
@@ -214,13 +286,14 @@ describe("POST /user/add-song", function () {
 });
 
 describe("GET /user/likes", function () {
-  it("User Likes page", async function () {
+  it("View the User's Liked Songs", async function () {
     const response = await request
       .get(`/user/likes`).set("Authorization", "Bearer " + token);
-    console.log(response);
+    console.log(response['text']);
 
     expect(response.status).to.eql(200);
 
+    console.log(response.songuri);
     //const attributes = response.body.data.attributes;
     //expect(attributes).to.include.keys("kilometers", "miles", "nautical_miles");
     //expect(attributes.kilometers).to.eql(8692.066508240026);
@@ -234,9 +307,8 @@ describe("POST /user/logout", function () {
     const response = await request
       .post(`/user/logout`).set("Authorization", "Bearer " + token);
     assert.equal(response['text'], "User Logged out!");
-    console.log(response)
+    console.log(response['text'])
     expect(response.status).to.eql(200);
-
     //const attributes = response.body.data.attributes;
     //expect(attributes).to.include.keys("kilometers", "miles", "nautical_miles");
     //expect(attributes.kilometers).to.eql(8692.066508240026);
@@ -245,7 +317,7 @@ describe("POST /user/logout", function () {
   });
 });
 describe("GET /session/create", function () {
-    it("should say Hello", async function(){
+    it("should say Hello. No longer logged in", async function(){
 	const response = await request.get("/session/create");
 	console.log(response['text']);
 	assert.equal(response['text'], "Hello");
